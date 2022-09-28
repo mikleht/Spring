b2deabc6453ba66a404fa98bcd94cd1a240a77a1
Mika Lehtimäki <mht.lehtimaki@gmail.com>
2022-09-28 19:58:32 +0300

    conditional property added

--- demo/src/main/java/com/example/demo/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
+	@Value( "${myprop}" )
+	private String myprop;
+
+	@Value( "${myotherprop:xx}" )
+	private String myotherprop;
+
+
 	@GetMapping("/")
-	public String hello() {		
+	public String hello() {
+		System.out.println("value: " + myprop);
+		System.out.println("conditional value: " + myotherprop);		
 		return "hello again";
 	}
 
